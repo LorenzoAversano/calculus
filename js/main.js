8092a922fa1a2b2e6acfeecf0ed3105aedcdded5
Lorenzo <lorenzo.aversano@edu.devinci.fr>
2022-03-02 12:57:34 +0100

    :white_check_mark: Finish the project !

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const els = { //Je liste tous les éléments que je veux utiliser et par défaut je les mets à 
     question: null,
-    questionCount: null,
     questionScreen: null,
     answerTitle: null,
     answerDetails: null,
@@ -9,56 +8,22 @@ const els = { //Je liste tous les éléments que je veux utiliser et par défaut
     answerQuestion: null,
     nextQuestionBtn: null,
 };
+
 let question = null;
-let questionNumber = 1; 
-let questionNumberTotal = 5; // On souhaite avoir 5 questions 
-let score = 0;
 
 const init = () => { //Cette constante me permet de prendre les id que j'ai crée dans ma page HTML
     els.question = document.querySelector('#question'); 
-    els.questionCount = document.querySelector('#question-count');
     els.questionScreen = document.querySelector('#question-screen');
     els.answerTitle = document.querySelector('#answer-title');
     els.answerQuestion = document.querySelector('#answer-question');
     els.answerDetails = document.querySelector('#answer-details');
-    els.answerQuestion = document.querySelector('#answer-question');
     els.answerScreen = document.querySelector('#answer-screen');
     els.possibilities = document.querySelector('#possibilities');
-    els.nextQuestionBtn = document.querySelector('#next');
 
-
-    showNewQuestion();
-    
 
     els.possibilities.addEventListener('click', ({ target }) => {
         giveAnswer(parseInt(target.innerHTML));
     });
-
-    els.nextQuestionBtn.addEventListener('click', () => {  
-        questionNumber++;
-        if (questionNumber <= questionNumberTotal) {
-            showNewQuestion();
-            els.questionScreen.style.display = 'block';
-            els.answerScreen.style.display = 'none';
-        } else {
-            els.questionScreen.style.display = 'none';
-            els.answerScreen.style.display = 'block';
-            els.nextQuestionBtn.style.display = 'none';
-
-            els.answerTitle.innerHTML = 'Fécilitations !';
-            els.answerQuestion.innerHTML = `Ton score est de : ${score} / ${questionNumberTotal}`;
-            els.answerDetails.innerHTML = '';
-            
-        }
-    });
-};
-
-const showNewQuestion = () => {
-    question = generateQuestion();
-    els.question.innerHTML = question.title;
-    els.possibilities.innerHTML = question.possibilities.map((possibility) => {
-        return `<li>${possibility}</li>`;
-    }).join('');
 };
 
 
@@ -68,7 +33,6 @@ const giveAnswer = (userAnswer) => {
         els.answerTitle.innerHTML = 'Bien joué !';
         els.answerQuestion.innerHTML = '';
         els.answerDetails.innerHTML = '';
-        score++;
     } else {
         els.answerTitle.innerHTML = 'Mauvaise réponse ';
         els.answerQuestion.innerHTML = question.title;
@@ -81,8 +45,3 @@ const giveAnswer = (userAnswer) => {
 window.onload = init; // Au chargement de la page on commence la fonction
 
 
-
-function myFunction() {
-	document.getElementById("question-screen").style.display = "block";
-    document.getElementById("start").style.display = "none";
-}
--- js/questions.js
+++ js/questions.js
@@ -1,36 +1,47 @@
-easy = document.getElementById('#easy');
-medium = document.getElementById('#medium');
-//Je crée une constante pour généer une question
-const generateQuestion = () => {
-    const question = {};
-
-
-    if (easy === "1") {
-        var operators = ["+", "-"]; //Je prends différents opérateurs, ici addition, soustraction et multiplication.
-        var operands = [ //On cherche la fonction getRandomInt qui me prends un chiffre au hasard entre 1 et 9
-            getRandomInt(1, 9),
-            getRandomInt(1, 9)
-        ];
+function difficulty(id) {
+	document.getElementById("question-screen").style.display = "block";
+    document.getElementById("start").style.display = "none";
+    if (id == 'easy') {
+        document.querySelector(".nexte").style.display = "block";
+    }
+    else if (id == 'medium'){
+        document.querySelector(".nextm").style.display = "block";
     } 
-    else if (medium=== "2") {
-        var operators = ["+", "-"];
-        var operands = [ //On cherche la fonction getRandomInt qui me prends un chiffre au hasard entre 1 et 9
-            getRandomInt(50, 59),
-            getRandomInt(51, 99)
-        ];
-    }
     else {
-        var operators = ["+", "-", "x" ];
-        var operands = [ //On cherche la fonction getRandomInt qui me prends un chiffre au hasard entre 1 et 9
-            getRandomInt(1, 30),
-            getRandomInt(1, 30)
-        ];
-    }
+        document.querySelector(".nexth").style.display = "block";
+    } 
+    generateQuestion(id);
+    showNewQuestion(id)
+}
 
-    var sOperator = operators[getRandomInt(0, operators.length - 1)]; //Je récupère au hasard un des opéarteurs
+
+//Je crée une constante pour généer une question
+const generateQuestion = (id) => {
+    const question = {};
+    var operators = ["+", "-" ];
+    var max = 0;
+
+    if(id == 'easy'){
+        max = 10; 
+    }
+    else if (id == 'medium'){
+        max = 10;
+        operators = ["+", "-", "x" ];
+
+    } 
+    else {
+        max = 20;
+        operators = ["+", "-", "x" ];
+
+    } 
+    var operands = [ //On cherche la fonction getRandomInt qui me prends un chiffre au hasard entre 1 et 9
+        getRandomInt(1, max),
+        getRandomInt(1, max),
+    ];
+
+    var sOperator = operators[getRandomInt(0, operators.length - 1)]; //Je récupère au hasard un des opérateurs
 
     //Je crée maintenant ma question qui va être afficher
-    
     question.title = `${operands[0]} ${sOperator} ${operands[1]}`;  // On prends l'opperand 0 soit le chiffre de gauche et on met un opérateur et enfin on prend l'opérand 1e soit le chiffre à gauche 
     var answer = getAnswer(operands, sOperator); // On affiche la réponse 
     question.answer = answer; 
@@ -42,12 +53,11 @@ const generateQuestion = () => {
 };
 
 
-
 //Cette constante a pour but d'afficher plusieurs possibilités
 var getPossibilities = (answer) => {
     var answers = [answer];
     for (let i = 0; answers.length < 4; i++) { //On ne veut que 4 possibilités 
-        const random = getRandomInt(-500, 500);
+        const random = getRandomInt(answer - 10, answer + 10);
         if (answers.includes(random) === false) {  
             answers.push(random);
         }
@@ -60,6 +70,14 @@ var getPossibilities = (answer) => {
     return answers;
 };
 
+
+const showNewQuestion = (id) => {
+    question = generateQuestion(id);
+    els.question.innerHTML = question.title;
+    els.possibilities.innerHTML = question.possibilities.map((possibility) => {
+        return `<li>${possibility}</li>`;
+    }).join('');
+};
 
 
 // Cette constante va nous permettre d'avoir la bonne réponse de chaque question avec les différents opérateurs possible.
@@ -83,10 +101,11 @@ const getAnswer = ([lOperator, rOperator], strOperator) => {
 const getRandomInt = (min, max) => { 
     min = Math.ceil(min);
     max = Math.floor(max);
+    // console.log(min + 'min')
+    // console.log(max +'max')
     return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
-
-
 
 
 
